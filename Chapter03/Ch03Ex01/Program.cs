68b7f196f3c20b968724609765613c99dc371a6b
dandrej13 <dimitrievsk.andrej@hotmail.com>
2022-09-22 01:59:10 +0200

    second repos

---
+++ Chapter03/Ch03Ex01/Program.cs
@@ -0,0 +1,7 @@
+﻿int myInteger;
+string myString;
+
+myInteger = 17;
+myString = "\"myInteger\" is";
+Console.WriteLine($"{myString} {myInteger}");
+Console.ReadKey(); 
---
+++ Chapter03/Ch03Ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ch03Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber, secondNumber;
+            string userName;
+            Console.WriteLine("Enter your name:");
+            userName = Console.ReadLine();
+            Console.WriteLine("Now give me a number:");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Now give me a second number:");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(
+                $"The sum of {firstNumber} and {secondNumber} is: " +
+                $"{firstNumber + secondNumber} \r\n" +
+                $"The sum of subtracting {firstNumber} and {secondNumber} is: " +
+                $"{firstNumber - secondNumber} \r\n" +
+                $"The sum of multyplying {firstNumber} and {secondNumber} is: " +
+                $"{firstNumber * secondNumber} \r\n" +
+                $"The sum of dividing {firstNumber} and {secondNumber} is: " +
+                $"{firstNumber / secondNumber}"
+                );
+            Console.WriteLine("Press ENTER to exit");
+        }
+    }
+}
